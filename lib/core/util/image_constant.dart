806a6b9e8f6532cfb8b2b35ce87cfb8594de5062
nhatcoi <nhatcoifacebook@gmail.com>
2025-08-23 11:03:46 +0700

    feat: add SVG icons and image constants for app assets

---
+++ lib/core/util/image_constant.dart
@@ -0,0 +1,23 @@
+class ImageConstant {
+
+  // base path
+  static final String _basePath = 'lib/assets/images/';
+
+  // Placeholder image for fallback
+  static String imgPlaceholder = '${_basePath}placeholder.png';
+
+  // Common Images
+  // Model Splash Model Screen
+  static String imgLogo = '${_basePath}icon/icon_logo.svg';
+  static String loadingSvg = '${_basePath}icon/icon_loading.svg';
+
+  // welcome
+  static String imgGoogleIcon = '${_basePath}icon/icon_google.svg';
+  static String imgBackground1 = '${_basePath}welcome/background-1.png';
+  static String imgBackground2 = '${_basePath}welcome/background-2.png';
+  static String imgBackground3 = '${_basePath}welcome/background-3.png';
+  static String imgBackground4 = '${_basePath}welcome/background-4.png';
+
+  // Custom Image View Screen
+  static String imgImageNotFound = '${_basePath}placeholder.png';
+}
